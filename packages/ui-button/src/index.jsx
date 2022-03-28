7527b794d724d91147136869442f22ec00783d03
Khanh Nguyen <khanh.nguyen@visual-meta.com>
2022-03-28 15:19:22 +0200

    Update

--- packages/ui-button/src/index.jsx
+++ packages/ui-button/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 
 const Button = ({ message = 'Hello world' }) => (
-   <button className="btn">{message + ' something else'}</button>
+   <button className="btn">{message + ' something else nothing'}</button>
 );
 
 export default Button;

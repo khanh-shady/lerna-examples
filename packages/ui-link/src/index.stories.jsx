6304b7d7cda1b2c210d1f4fb2e0d830b5e109314
Khanh Nguyen <khanh.nguyen@visual-meta.com>
2022-03-28 12:05:08 +0200

    Update

--- packages/ui-link/src/index.stories.jsx
+++ packages/ui-link/src/index.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from './index.js';
+import Link from './index.jsx';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default { title: 'Link component', decorators: [ withKnobs ] };

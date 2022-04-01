489c60df15262e70ed63c4e9457afb8e2fcf96a0
Khanh Nguyen <khanh.nguyen@visual-meta.com>
2022-04-01 11:51:59 +0200

    Update

--- packages/ui-button/src/index.jsx
+++ packages/ui-button/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import './index.scss';
+import './index.scss';
 import * as styles from './index.scss';
 
 const Button = ({ message = 'Hello world' }) => (
-   // <button className="btn">{message + ' something else nothing else 3'}</button>
-   <button className={styles.btn}>{message + ' something else nothing else 3'}</button>
+   <button className="btn">{message + ' something else nothing else 3'}</button>
+   // <button className={styles.btn}>{message + ' something else nothing else 3'}</button>
 );
 
 export default Button;

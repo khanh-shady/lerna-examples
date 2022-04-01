dff3c412ba90dd3d6df4a41802b96e38d328508b
Khanh Nguyen <khanh.nguyen@visual-meta.com>
2022-04-01 14:56:54 +0200

    Update

--- packages/ui-link/src/index.jsx
+++ packages/ui-link/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './index.scss';
+import * as styles from './index.scss';
 
 const Link = ({ message = 'Hello world', href="/" }) => (
-   <a className="link" href={href} target="_blank">{message + "1"}</a>
+   <a className={styles.btn} href={href} target="_blank">{message + "1"}</a>
 );
 
 export default Link;

02c73502709497a94a0148afe61e0059a6e3f9d5
Khanh Nguyen <khanh.nguyen@visual-meta.com>
2022-04-05 14:55:57 +0200

    Update

--- packages/ui-base/src/index.jsx
+++ packages/ui-base/src/index.jsx
@@ -1,5 +1,5 @@
-import Button from '@khanh.nguyen95/ui-button';
-import H1 from '@khanh.nguyen95/ui-h1';
-import Link from '@khanh.nguyen95/ui-link';
+import Button from '@search/ui-button';
+import H1 from '@search/ui-h1';
+import Link from '@search/ui-link';
 
 export { Button, H1, Link };
